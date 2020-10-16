80ba7de854534149aeff701a77d08964bd0c16d0
rustamsafarovrs <rs.programmer@hotmail.com>
2020-10-16 15:30:34 +0500

    force commit

--- src/tj/rs/devteam/Main.java
+++ src/tj/rs/devteam/Main.java
@@ -1,8 +1,33 @@
 package tj.rs.devteam;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Scanner s = new Scanner(System.in);
+        int n = 10;
+        System.out.print("input array size: ");
+        n = s.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = s.nextInt();
+//            System.out.println(array[i]);
+        }
+
+        System.out.println(maxInArray(array));
+    }
+
+    public static int maxInArray(int[] array) {
+        int max = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
     }
 }
